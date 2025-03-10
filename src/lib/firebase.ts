e7518d3dea72781366f7e8eeb59b76c64628f4c1
Laiane Suzart <laiane@pm.me>
2025-03-10 00:06:45 -0300

    add firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,26 +1,105 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { doc, getFirestore, onSnapshot } from "firebase/firestore";
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { derived, writable, type Readable } from "svelte/store";
 import {
-  FB_API_KEY,
-  FB_DOMAIN,
-  FB_PROJECT,
-  FB_BUCKET,
-  FB_SENDER_ID,
-  FB_APP_ID,
-} from "$env/static/private";
+  PUBLIC_FB_API_KEY,
+  PUBLIC_FB_DOMAIN,
+  PUBLIC_FB_PROJECT,
+  PUBLIC_FB_BUCKET,
+  PUBLIC_FB_SENDER_ID,
+  PUBLIC_FB_APP_ID,
+} from "$env/static/public";
 
 const firebaseConfig = {
-  apiKey: FB_API_KEY,
-  authDomain: FB_DOMAIN,
-  projectId: FB_PROJECT,
-  storageBucket: FB_BUCKET,
-  messagingSenderId: FB_SENDER_ID,
-  appId: FB_APP_ID,
+  apiKey: PUBLIC_FB_API_KEY,
+  authDomain: PUBLIC_FB_DOMAIN,
+  projectId: PUBLIC_FB_PROJECT,
+  storageBucket: PUBLIC_FB_BUCKET,
+  messagingSenderId: PUBLIC_FB_SENDER_ID,
+  appId: PUBLIC_FB_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth();
 export const storage = getStorage();
+
+/**
+ * @returns a store with the current firebase user
+ */
+function userStore() {
+  let unsubscribe: () => void;
+
+  if (!auth || !globalThis.window) {
+    console.warn("Auth is not initialized or not in browser");
+    const { subscribe } = writable<User | null>(null);
+    return {
+      subscribe,
+    };
+  }
+
+  const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+    unsubscribe = onAuthStateChanged(auth, (user) => {
+      set(user);
+    });
+
+    return () => unsubscribe();
+  });
+
+  return {
+    subscribe,
+  };
+}
+
+export const user = userStore();
+
+/**
+ * @param  {string} path document path or reference
+ * @returns a store with realtime updates on document data
+ */
+export function docStore<T>(path: string) {
+  let unsubscribe: () => void;
+
+  const docRef = doc(db, path);
+
+  const { subscribe } = writable<T | null>(null, (set) => {
+    unsubscribe = onSnapshot(docRef, (snapshot) => {
+      set((snapshot.data() as T) ?? null);
+    });
+
+    return () => unsubscribe();
+  });
+
+  return {
+    subscribe,
+    ref: docRef,
+    id: docRef.id,
+  };
+}
+
+interface Link {
+  icon: string;
+  title: string;
+  url: string;
+}
+
+interface UserData {
+  username: string;
+  bio: string;
+  photoURL: string;
+  links: Link[];
+  published: boolean;
+}
+
+export const userData: Readable<UserData | null> = derived(
+  user,
+  ($user, set) => {
+    if ($user) {
+      return docStore<UserData>(`users/${$user.uid}`).subscribe(set);
+    } else {
+      set(null);
+    }
+  }
+);
